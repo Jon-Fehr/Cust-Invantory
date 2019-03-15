daf2d6cc7e9ecd091aebdcfb66f9fc26c7611839
Jon-Fehr <41446236+Jon-Fehr@users.noreply.github.com>
2019-03-15 17:28:28 -0500

    Started adding the basic items
    Added four buttons for the basic controls of the application.

--- Cust-Inventory/Cust-Inventory/Form1.Designer.cs
+++ Cust-Inventory/Cust-Inventory/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnUsers = new System.Windows.Forms.Button();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(0, 61);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(142, 36);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnUsers
+            // 
+            this.btnUsers.Location = new System.Drawing.Point(0, 103);
+            this.btnUsers.Name = "btnUsers";
+            this.btnUsers.Size = new System.Drawing.Size(142, 38);
+            this.btnUsers.TabIndex = 1;
+            this.btnUsers.Text = "Users";
+            this.btnUsers.UseVisualStyleBackColor = true;
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.Location = new System.Drawing.Point(0, 147);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(142, 39);
+            this.btnCustomer.TabIndex = 2;
+            this.btnCustomer.Text = "Customers";
+            this.btnCustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Location = new System.Drawing.Point(0, 192);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(142, 42);
+            this.btnReports.TabIndex = 3;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1376, 793);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnCustomer);
+            this.Controls.Add(this.btnUsers);
+            this.Controls.Add(this.btnHome);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yakesh Steel Inventory Program";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnUsers;
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnReports;
     }
 }
 
