73f39c2b83cd9cf598f018b3dc2602692f8b0bed
Jon-Fehr <41446236+Jon-Fehr@users.noreply.github.com>
2019-03-18 16:22:18 -0500

    User Control
    Added user controls - Changed the background color so I can test if buttons work.

--- Cust-Inventory/Cust-Inventory/Form1.Designer.cs
+++ Cust-Inventory/Cust-Inventory/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.btnUsers = new System.Windows.Forms.Button();
             this.btnCustomer = new System.Windows.Forms.Button();
             this.btnReports = new System.Windows.Forms.Button();
+            this.customersControl1 = new Cust_Inventory.CustomersControl();
+            this.homeControl1 = new Cust_Inventory.HomeControl();
+            this.reportsControl1 = new Cust_Inventory.ReportsControl();
+            this.usersControl1 = new Cust_Inventory.UsersControl();
             this.SuspendLayout();
             // 
             // btnHome
@@ -70,12 +74,60 @@
             this.btnReports.Text = "Reports";
             this.btnReports.UseVisualStyleBackColor = true;
             // 
+            // customersControl1
+            // 
+            this.customersControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customersControl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.customersControl1.Location = new System.Drawing.Point(148, 0);
+            this.customersControl1.Name = "customersControl1";
+            this.customersControl1.Size = new System.Drawing.Size(1229, 582);
+            this.customersControl1.TabIndex = 4;
+            // 
+            // homeControl1
+            // 
+            this.homeControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.homeControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.homeControl1.Location = new System.Drawing.Point(148, 0);
+            this.homeControl1.Name = "homeControl1";
+            this.homeControl1.Size = new System.Drawing.Size(1229, 582);
+            this.homeControl1.TabIndex = 5;
+            // 
+            // reportsControl1
+            // 
+            this.reportsControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reportsControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.reportsControl1.Location = new System.Drawing.Point(148, 0);
+            this.reportsControl1.Name = "reportsControl1";
+            this.reportsControl1.Size = new System.Drawing.Size(1229, 582);
+            this.reportsControl1.TabIndex = 6;
+            // 
+            // usersControl1
+            // 
+            this.usersControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.usersControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.usersControl1.Location = new System.Drawing.Point(148, 0);
+            this.usersControl1.Name = "usersControl1";
+            this.usersControl1.Size = new System.Drawing.Size(1229, 582);
+            this.usersControl1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(1376, 793);
+            this.Controls.Add(this.usersControl1);
+            this.Controls.Add(this.reportsControl1);
+            this.Controls.Add(this.homeControl1);
+            this.Controls.Add(this.customersControl1);
             this.Controls.Add(this.btnReports);
             this.Controls.Add(this.btnCustomer);
             this.Controls.Add(this.btnUsers);
@@ -93,6 +145,10 @@
         private System.Windows.Forms.Button btnUsers;
         private System.Windows.Forms.Button btnCustomer;
         private System.Windows.Forms.Button btnReports;
+        private CustomersControl customersControl1;
+        private HomeControl homeControl1;
+        private ReportsControl reportsControl1;
+        private UsersControl usersControl1;
     }
 }
 
